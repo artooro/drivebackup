3b51d1896999ba20a2867140c319188262d2b325
Arthur Wiebe <arthur@artooro.com>
2017-02-16 13:09:41 -0500

    added support for ARMv7, tested on a QNAP NAS.

--- cross_build.sh
+++ cross_build.sh
@@ -18,6 +18,12 @@ export GOARCH="386"
 go build -v -o pkg/drivebackup.linux-386/drivebackup
 tar -czvf pkg/drivebackup.linux-386.tar.gz pkg/drivebackup.linux-386
 
+echo "Building linux.armv7"
+export GOARM=7
+export GOARCH=arm
+go build -v -o pkg/drivebackup.linux-armv7/drivebackup
+tar -czvf pkg/drivebackup.linux-armv7.tar.gz pkg/drivebackup.linux-armv7
+
 unset GOOS
 unset GOARCH
 echo "Cross build complete"
